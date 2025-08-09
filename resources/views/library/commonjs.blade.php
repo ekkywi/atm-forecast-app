da5e8a0eccc63a5e8dfc098b3d411e13679984a3
Yon Ekky Wijayanto <yonekkywijayanto@outlook.com>
2025-08-09 12:22:14 +0700

    update

---
+++ resources/views/library/commonjs.blade.php
@@ -0,0 +1,20 @@
+ <div class="scrollToTop">
+     <span class="arrow"><i class="ri-arrow-up-s-fill fs-20"></i></span>
+ </div>
+ <div id="responsive-overlay"></div>
+
+ <script src="{{ asset("libs/@popperjs/core/umd/popper.min.js") }}"></script>
+ <script src="{{ asset("libs/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+ <script src="{{ asset("js/defaultmenu.min.js") }}"></script>
+ <script src="{{ asset("libs/node-waves/waves.min.js") }}"></script>
+ <script src="{{ asset("js/sticky.js") }}"></script>
+ <script src="{{ asset("libs/simplebar/simplebar.min.js") }}"></script>
+ <script src="{{ asset("js/simplebar.js") }}"></script>
+ <script src="{{ asset("libs/@simonwep/pickr/pickr.es5.min.js") }}"></script>
+ <script src="{{ asset("libs/jsvectormap/js/jsvectormap.min.js") }}"></script>
+ <script src="{{ asset("libs/jsvectormap/maps/world-merc.js") }}"></script>
+ <script src="{{ asset("libs/apexcharts/apexcharts.min.js") }}"></script>
+ <script src="{{ asset("libs/chart.js/chart.min.js") }}"></script>
+ <script src="{{ asset("js/crm-dashboard.js") }}"></script>
+ <script src="{{ asset("js/custom-switcher.min.js") }}"></script>
+ <script src="{{ asset("js/custom.js") }}"></script>
